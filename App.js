c32d2cd4410628f0bfdc344ab14f42bad9a54652
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-10-19 11:51:17 +0700

    Function Delete Item

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import TodoItem from './components/TodoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,12 @@ export default function App() {
     { text: 'create an app', key: '2'},
     { text: 'play on the switch', key: '3'}
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key !== key);
+    });
+  }
 
   return (
     <View style={styles.container}>
@@ -18,7 +25,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
           />
         </View>
